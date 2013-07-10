c5143b93e927d0ba8f418cd7f11589ffb0604bce
tblakey89 <tomblakey89@googlemail.com>
2013-07-10 22:52:41 +0100

    add password activeadmin area

--- app/admin/passwords.rb
+++ app/admin/passwords.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Password do
-  
+
+  config.filters = false
+
+  controller do
+    actions :all, except: [:destroy, :new, :show]
+  end
+
+  index do
+    column :password
+    default_actions
+  end
 end
